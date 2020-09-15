7d4bff28ac0a484b3a334bc11fc77977285137ca
Tham Minh Triet Nguyen <mt.hue1991@gmail.com>
2020-09-15 22:14:59 +0700

    .

--- index.js
+++ index.js
@@ -81,22 +81,25 @@ app.post('/webhook', (req, res) => {
                 // keep track of each user by their senderId
                 console.log('1');
                 const senderId = event.sender.id
-                if (!users[senderId] || !users[senderId].currentState) {
+                const userSenderId = users[senderId];
+                if (!userSenderId || !userSenderId.currentState) {
                     console.log('2');
-                    users[senderId] = {};
-                    users[senderId].currentState = states.question1;
+                    userSenderId = {};
+                    userSenderId.currentState = states.question1;
                 } else {
                     // store the answer and update the state
-                    users[senderId][users[senderId].currentState] = event.message.text
-                    users[senderId].currentState = nextStates[users[senderId.currentState]]
+                    userSenderId[userSenderId.currentState] = event.message.text
+                    userSenderId.currentState = nextStates[userSenderId.currentState];
                     console.log("3 =====");
                     console.log(users);
                     console.log(event)
+                    console.log(nextStates)
+                    console.log(" =====");
                 }
                 console.log('4');
                 // send a message to the user via the Messenger API
-                if (messages[users[senderId].currentState]) {
-                    sendTextMessage(senderId, messages[users[senderId].currentState])
+                if (messages[userSenderId.currentState]) {
+                    sendTextMessage(senderId, messages[userSenderId.currentState])
                 }
             });
         });
