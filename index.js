1027d7e2bad95b4ccfa653740b055ac1ee75d854
Tham Minh Triet Nguyen <mt.hue1991@gmail.com>
2020-09-15 15:33:04 +0700

    Correct verify token

--- index.js
+++ index.js
@@ -53,13 +53,12 @@ const nextStates = {
 }
 
 app.get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN || 'EAAFVCXifkIkBAMl1TUueixS6CkypIbwBrOPl9VxghpkPfkMTmHkKl2a3L0ydRpUedMDbgcGKwRZCf0VZCzwinBkdSUYyS0jQkMttaKMEeoX4lPFZAbhlklE1bg9wygFnuBVea8LqRwpDFnahAHgFzZCMydjuISe0cpXOpmFJFgZDZD';
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN || '8cc5b87fa1dca8b34622499f2039dde0';
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
     if (mode && token) {
-
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
             console.log('WEBHOOK_VERIFIED');
             res.status(200).send(challenge);
