3c41ab8bbd0d40648623d993d321be7dae603285
Tham Minh Triet Nguyen <mt.hue1991@gmail.com>
2020-09-20 12:22:04 +0700

    .

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const request = require("request"),
   body_parser = require("body-parser"),
   app = express().use(body_parser.json());
 
-require("./controllers/webhook");
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const FACEBOOK_GRAPH_API_BASE_URL = process.env.PAGE_ACCESS_TOKEN;
 
 const mode = process.env.MODE
 const host = process.env.HOST;
